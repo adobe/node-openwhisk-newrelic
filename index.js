d0ba2083a4acab2ebb2b3ca417780ea4cae893d6
Bob van Manen <bmanen@adobe.com>
2019-03-27 10:15:42 -0700

    update

--- index.js
+++ index.js
@@ -17,10 +17,34 @@
 
 'use strict';
 
-const { Agent } = require('./lib/agent');
+const { Agent } = require('./lib/newrelic');
 const { Sampler } = require('./lib/sampler');
 
 module.exports = {
     Agent,
+    Sampler
+}
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2019 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+'use strict';
+
+const { Sampler } = require('./lib/sampler');
+
+module.exports = {
     Sampler
 }
