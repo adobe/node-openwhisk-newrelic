fd293f82aa86a418dcdf1e90b76c25fa2de09e8a
Bob van Manen <bmanen@adobe.com>
2019-03-28 10:26:32 -0700

    add async-worker-sampler-object

--- test/instrument.test.js
+++ test/instrument.test.js
@@ -196,4 +196,35 @@ describe("instrument", () => {
         assert.strictEqual(flatten.name_q3, 6.25);
         assert.strictEqual(flatten.name_result, 1700);
     })
+    it("async-worker-sampler-object", async () => {
+        const metrics = {};
+        let counter = 0;
+        const result = await instrument({
+            execute: asyncTimeout,
+            metrics: (_, result) => {
+                return { result }
+            },
+            sample: () => {
+                return { value: ++counter };
+            },
+            sampleInterval: 200
+        }, metrics, "name").execute(1700);
+        assert.strictEqual(result, 1700);
+        const flatten = Metrics.flatten(metrics);
+        assert.deepStrictEqual(Object.getOwnPropertyNames(flatten), [
+            "name_start", "name_end", "name_duration", 
+            "name_value_min", "name_value_max", "name_value_mean", "name_value_stdev", 
+            "name_value_median", "name_value_q1", "name_value_q3", 
+            "name_result"
+        ]);
+        assert.strictEqual(flatten.name_end - flatten.name_start, flatten.name_duration);
+        assert.strictEqual(flatten.name_value_min, 1);
+        assert.strictEqual(flatten.name_value_max, 8);
+        assert.strictEqual(flatten.name_value_mean, 4.5);
+        assert.strictEqual(Math.round(flatten.name_value_stdev * 1000) / 1000, 2.449);
+        assert.strictEqual(flatten.name_value_median, 4.5);
+        assert.strictEqual(flatten.name_value_q1, 2.75);
+        assert.strictEqual(flatten.name_value_q3, 6.25);
+        assert.strictEqual(flatten.name_result, 1700);
+    })
 });
