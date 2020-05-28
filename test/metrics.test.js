d6711e51e43191ff55f7a917ecfa7454633e339e
dhendric <dhendric@adobe.com>
2020-05-28 17:43:09 -0600

    Removed Node versions 10.15 and 10.19 from Travis Modified Travis to run only on Linux Added code coverage reporting Simplified script test Updated eslint version and fixed complaints Fixed script line in jobs for Travis

--- test/metrics.test.js
+++ test/metrics.test.js
@@ -1,4 +1,15 @@
 /*
+ * Copyright 2020 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+/*
 Copyright 2020 Adobe. All rights reserved.
 This file is licensed to you under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License. You may obtain a copy
@@ -98,11 +109,11 @@ describe("metrics", () => {
         it("unsupported property type: function", () => {
             try {
                 Metrics.flatten({ value: function() {
-                    console.log('Scary function!')
+                    console.log('Scary function!');
                 }});
                 assert.fail('Should have failed');
             } catch(e) {
-                assert(e.message.includes("Unsupported property"))
+                assert(e.message.includes("Unsupported property"));
             }
         });
         it("number", () => {
@@ -211,7 +222,7 @@ describe("metrics", () => {
         });
         it("not-iterable", () => {
             try {
-                const x = 5
+                const x = 5;
                 for (const a of x) { a; }
             } catch (e) {
                 const flatten = Metrics.flatten({ e });
@@ -220,6 +231,6 @@ describe("metrics", () => {
                     e_name: "TypeError"
                 });
             }
-        })
+        });
     });
-})
+});
